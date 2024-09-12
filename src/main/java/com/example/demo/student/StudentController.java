aaec67111461482a591a5f8bcc2792ded1994129
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-09-12 20:33:13 +0800

    feat(db): put student

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -32,6 +32,14 @@ public class StudentController {
     @DeleteMapping(path = "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long studentId) {
         studentService.deleteStudent(studentId);
+    }
 
+    // update student
+    @PutMapping(path="{studentId}")
+    public void updateStudent(
+            @PathVariable ("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,10 +2,12 @@ package com.example.demo.student;
 
 // where the business logic lies
 
+import jakarta.transaction.Transactional; // should use jakarta?
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service // spring bean that has to be instantiated
@@ -38,5 +40,29 @@ public class StudentService {
             throw new IllegalStateException("student with id " + studentId + " does not exist in db");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional // when we use this annotation, we dont have to use jbql query to update the student
+    public void updateStudent(Long studentId, String name, String email) {
+        // put request to update student details
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("student with id " + studentId + " does not exist in db"));
+
+        if (name != null &&
+                !name.isEmpty() &&
+                // check if its not the same name as the one in db
+                !Objects.equals(student.getName(), name)
+        ) {
+            student.setName(name);
+        }
+        if (email != null && !email.isEmpty() &&
+                // check if its not the same name as the one in db
+                !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
