396f3ccb1379748aeda2790a3e96a6bbed3fda1a
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-09-12 17:50:03 +0800

    feat(db): post student - check if email exists

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -5,8 +5,16 @@ package com.example.demo.student;
 // anything that access the database with jpa
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository // similar to @Service, @Component, @Controller - its called Repository because its responsible for data access
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    // find a user by email
+    // this translates to SELECT * FROM student WHERE email = ?
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -5,6 +5,7 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service // spring bean that has to be instantiated
 public class StudentService {
@@ -21,6 +22,12 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        // if the email is present, then throw exception
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("Email taken");
+        }
+        studentRepository.save(student);
     }
 }
